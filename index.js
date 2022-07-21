71dddb69987e699e3c5787b50c2c5574bd87b552
tismenetski <tismenetski@gmail.com>
2022-07-21 09:55:58 +0300

    added morgan and middleware

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ require('dotenv').config()
 require('express-async-errors');
 const express = require('express')
 const db = require('./db/connectDB')
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
 const authRouter = require('./routes/authRoutes')
 const clientRouter = require('./routes/clientRoutes')
 const employeeRouter = require('./routes/employeeRoutes')
-
+const categoryRouter = require('./routes/categoryRoutes')
+const productRouter = require('./routes/productRoutes')
 
 const app = express()
-
+app.use(morgan('tiny'))
 app.use(express.json());
+app.use(fileUpload());
+
 
 const port = process.env.PORT || 6060;
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/client', clientRouter)
 app.use('/api/v1/employee', employeeRouter)
+app.use('/api/v1/category', categoryRouter)
+app.use('/api/v1/product', productRouter)
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const start = async() => {
 
